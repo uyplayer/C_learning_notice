706caa62a65908b5c5ee17eba577d372cf60b910
uyplayer <32848979@qq.com>
2020-08-21 18:21:55 +0800

    c code

---
+++ OS/intro/cpu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+	fprintf(stderr, "usage: cpu <string>\n"); // stderr 标准输出给终端上
+	exit(1);
+    }
+    char *str = argv[1];
+
+    while (1) {
+	printf("%s\n", str);
+	Spin(1);
+    }
+    return 0;
+}
